d01bc4379b85f0a5d170ede03284726ce8c0271a
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-07 11:17:16 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html = urlopen('http://pythonscraping.com/pages/page1.html')
+bs = BeautifulSoup(html.read(), 'html.parser')
+print(bs.h1)
