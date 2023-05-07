529982948c8fd446e3c02aff2e36b5cc7282f9f7
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-07 11:21:14 -0300

    add exceptions handler

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+try:
+    # html = urlopen('http://pythonscraping.com/pages/page1.html')
+    html = urlopen('http://pythonscraping-thisurldoesnotexist.com/')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print('This server could not be found!')
+else:
+    print('It worked!')
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    print(bs.h1)
